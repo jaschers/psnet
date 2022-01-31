ff9eaf8f3866256d3c8ee1e522043ffad28fe0aa
Jann Aschersleben <Jann.Aschersleben@rwth-aachen.de>
2022-01-31 10:38:32 +0100

    float ps + uncomment filter plotting

--- scripts/cnn/cnn_evaluation.py
+++ scripts/cnn/cnn_evaluation.py
@@ -34,7 +34,7 @@ parser.add_argument("-i", "--input", type = str, required = True, metavar = "-",
 parser.add_argument("-na", "--name", type = str, required = False, metavar = "-", help = "Name of this particular experiment(s)", action='append', nargs='+')
 parser.add_argument("-l", "--label", type = str, required = False, metavar = "-", help = "plotting label for the individual experiments", action='append', nargs='+')
 parser.add_argument("-pt", "--particle_type", type = str, metavar = "-", choices = ["gamma", "gamma_diffuse", "proton"], help = "particle type [gamma, gamma_diffuse, proton], default: gamma", default = "gamma")
-parser.add_argument("-er", "--energy_range", type = float, required = False, metavar = "-", help = "set energy range of events in TeV, default: 0.02 300", default = [0.02, 300], nargs = 2)
+parser.add_argument("-er", "--energy_range", type = float, required = False, metavar = "-", help = "set energy range of events in TeV, default: 0.5 100", default = [0.5, 100], nargs = 2)
 parser.add_argument("-gl", "--gammaness_limit", type = float, required = False, metavar = "-", help = "separation: set min / max limit for reconstructed gammaness to investigate wrongly classified gamma/proton events [g_min (gamma), g_max (gamma), g_min (proton), g_max (proton)], default: 0.0 0.0 0.0 0.0", default = [0.0, 0.0, 0.0, 0.0], nargs = 4)
 parser.add_argument("-a", "--attribute", type = int, metavar = "-", choices = np.arange(0, 19, dtype = int), help = "attribute [0, 1 ... 18] (two required), default: 9 0", default = [9, 0], nargs = 2)
 parser.add_argument("-dl", "--domain_lower", type = int, metavar = "-", help = "Granulometry: domain - start at <value> <value>, default: 0 0", default = [0, 0], nargs = 2)
@@ -86,7 +86,7 @@ for i in range(len(args.input[0])):
     if args.input[0][i] == "ps":
         string_summary += f"\nInput: pattern spectra \nParticle type: {args.particle_type} \nEnergy range: {args.energy_range} \nAttribute: {args.attribute} \nDomain lower: {args.domain_lower} \nDomain higher: {args.domain_higher} \nMapper: {args.mapper} \nSize: {args.size} \nFilter: {args.filter}\n"
         string_input = np.append(string_input, "pattern_spectra")
-        string_input_short = np.append(string_input_short, "_ps")
+        string_input_short = np.append(string_input_short, "_ps_float")
         string_ps_input = np.append(string_ps_input, f"a_{args.attribute[0]}_{args.attribute[1]}__dl_{args.domain_lower[0]}_{args.domain_lower[1]}__dh_{args.domain_higher[0]}_{args.domain_higher[1]}__m_{args.mapper[0]}_{args.mapper[1]}__n_{args.size[0]}_{args.size[1]}__f_{args.filter}/")
         string_table_column = np.append(string_table_column, "pattern spectrum")
         string_data_type = np.append(string_data_type, "")
@@ -115,19 +115,19 @@ for i in range(len(args.input[0])):
 
     ####################################### Filters and feature maps ########################################
     # define example run and load example data
-    run = 107
-    X, Y = LoadExampleData(run, string_input[i], args.particle_type, string_ps_input[i], string_input_short[i], string_data_type[i], string_table_column[i])
+    # run = 107
+    # X, Y = LoadExampleData(run, string_input[i], args.particle_type, string_ps_input[i], string_input_short[i], string_data_type[i], string_table_column[i])
 
     # load model
     model_path = f"dm-finder/cnn/{string_input[i]}/{args.mode}/model/" + string_ps_input[i] + "model" + string_data_type[i] + string_name[i] + ".h5"
     model = keras.models.load_model(model_path)
     
     # plot filters
-    PlotFilters(model, f"dm-finder/cnn/{string_input[i]}/{args.mode}/results/" + string_ps_input[i] + f"{string_name[i][1:]}/" + "filters")
+    # PlotFilters(model, f"dm-finder/cnn/{string_input[i]}/{args.mode}/results/" + string_ps_input[i] + f"{string_name[i][1:]}/" + "filters")
 
-    # plot feature maps for an example image
-    index_example = 39
-    PlotFeatureMaps(X, model, index_example, f"dm-finder/cnn/{string_input[i]}/{args.mode}/results/" + string_ps_input[i] + f"{string_name[i][1:]}/" + "feature_maps")
+    # # plot feature maps for an example image
+    # index_example = 39
+    # PlotFeatureMaps(X, model, index_example, f"dm-finder/cnn/{string_input[i]}/{args.mode}/results/" + string_ps_input[i] + f"{string_name[i][1:]}/" + "feature_maps")
     ##########################################################################################
 
     # load data file that contains E_true and E_rec from the test set
