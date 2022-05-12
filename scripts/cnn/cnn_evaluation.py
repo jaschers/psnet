3fffa88b0aae4484463f1a8f724e0eb224ba1f0a
Jann Aschersleben <Jann.Aschersleben@rwth-aachen.de>
2022-05-12 16:42:11 +0200

    design updates

--- scripts/cnn/cnn_evaluation.py
+++ scripts/cnn/cnn_evaluation.py
@@ -16,7 +16,7 @@ import logging
 import time
 
 plt.rcParams.update({'font.size': 14})
-plt.rcParams.update({'font.family':'Times New Roman'}) #serif
+plt.rcParams.update({'font.family':'serif'}) #serif
 plt.rcParams["mathtext.fontset"] = 'dejavuserif' #dejavuserif
 pd.options.mode.chained_assignment = None 
 
