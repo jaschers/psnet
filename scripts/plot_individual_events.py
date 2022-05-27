53cc44d1a0061410b1e877ea455992f8afc92255
Jann Aschersleben <Jann.Aschersleben@rwth-aachen.de>
2022-05-27 10:06:47 +0200

    design changes

--- scripts/plot_individual_events.py
+++ scripts/plot_individual_events.py
@@ -5,7 +5,14 @@ import pandas as pd
 import argparse
 import os
 
-plt.rcParams.update({'font.size': 16})
+plt.rcParams.update({'font.size': 8})
+plt.rc('text', usetex=True )
+plt.rc('font', family='Times New Roman')#, weight='normal', size=14)
+plt.rcParams['mathtext.fontset'] = 'cm'
+
+cm_conversion_factor = 1/2.54  # centimeters in inches
+single_column_fig_size = (8.85679 * cm_conversion_factor, 8.85679 * 3/4 * cm_conversion_factor)
+single_column_half_fig_size = (8.85679 * cm_conversion_factor / 2, 8.85679 * 3/4 * cm_conversion_factor / 2)
 
 ######################################## argparse setup ########################################
 script_version=0.1
@@ -66,6 +73,8 @@ elif args.input == "ps":
 run_filename = f"{args.particle_type}_20deg_0deg_run{args.run}___cta-prod5-paranal_desert-2147m-Paranal-dark_merged.DL1"
 input_filename = f"dm-finder/cnn/{string_input}/input/{args.particle_type}/" + string_ps_input + run_filename + string_input_short + ".h5"
 
+attribute_names = [r"$A$ (log scale)", "Area of the minimum enclosing rectangle", "Length of the diagonal of the minimum encl. rect.", "Area (Peri)", "Perimeter (Peri)", "Complexity (Peri)", "Simplicity (Peri)", "Compactness (Peri)", r"$I$", r"$I / A^2$ (log scale)", "Compactnes", r"$I / A^2$ (Jagged)", "Jaggedness", "Entropy", "Lambda-Max", "Max. Pos. X", "Max. Pos. Y", "Grey level", "Sum Grey Levels"]
+
 table = pd.read_hdf(input_filename)
 
 if args.tel_id == None:
@@ -78,30 +87,30 @@ table.reset_index(inplace = True)
 print(table)
 
 if args.input == "ps":
-    path_tif = f"dm-finder/data/{args.particle_type}/pattern_spectra" + f"/a_{args.attribute[0]}_{args.attribute[1]}__dl_{args.domain_lower[0]}_{args.domain_lower[1]}__dh_{args.domain_higher[0]}_{args.domain_higher[1]}__m_{args.mapper[0]}_{args.mapper[1]}__n_{args.size[0]}_{args.size[1]}__f_{args.filter}/" + run_filename + "/float" + f"/{args.telescope_mode}" + "/obs_id_" + f"{table['obs_id'][0]}/tif/"
+    path_pdf = f"dm-finder/data/{args.particle_type}/pattern_spectra" + f"/a_{args.attribute[0]}_{args.attribute[1]}__dl_{args.domain_lower[0]}_{args.domain_lower[1]}__dh_{args.domain_higher[0]}_{args.domain_higher[1]}__m_{args.mapper[0]}_{args.mapper[1]}__n_{args.size[0]}_{args.size[1]}__f_{args.filter}/" + run_filename + "/float" + f"/{args.telescope_mode}" + "/obs_id_" + f"{table['obs_id'][0]}/pdf/"
 
     if args.tel_id == None:
-        filename_tif = "obs_id_" + f"{table['obs_id'][0]}" + "__event_id_" + f"{table['event_id'][0]}"
+        filename_pdf = "obs_id_" + f"{table['obs_id'][0]}" + "__event_id_" + f"{table['event_id'][0]}"
     else:
-        filename_tif = "obs_id_" + f"{table['obs_id'][0]}" + "__event_id_" + f"{table['event_id'][0]}" + "__tel_id_" + f"{table['tel_id'][0]}"
+        filename_pdf = "obs_id_" + f"{table['obs_id'][0]}" + "__event_id_" + f"{table['event_id'][0]}" + "__tel_id_" + f"{table['tel_id'][0]}"
         
 
-    os.makedirs(path_tif, exist_ok = True)
+    os.makedirs(path_pdf, exist_ok = True)
 
-    plt.figure()
+    plt.figure(figsize = single_column_fig_size)
 
     if args.tel_id == None:
-        plt.title("obs " + f"{table['obs_id'][0]}" + " - event " + f"{table['event_id'][0]}", fontsize = 16, pad = 20)
+        plt.title("obs " + f"{table['obs_id'][0]}" + " - event " + f"{table['event_id'][0]}", pad = 20)
     else:
-        plt.title("obs " + f"{table['obs_id'][0]}" + " - event " + f"{table['event_id'][0]}" + " - tel " + f"{table['tel_id'][0]}", fontsize = 16, pad = 20)
+        plt.title("obs " + f"{table['obs_id'][0]}" + " - event " + f"{table['event_id'][0]}" + " - tel " + f"{table['tel_id'][0]}", pad = 20)
 
     plt.imshow(table["pattern spectrum"][0], cmap = "Greys_r")
     plt.annotate('', xy=(0, -0.05), xycoords='axes fraction', xytext=(1, -0.05), arrowprops=dict(arrowstyle="<-", color='black'))
     plt.annotate('', xy=(-0.05, 1), xycoords='axes fraction', xytext=(-0.05, 0), arrowprops=dict(arrowstyle="<-", color='black'))
-    plt.xlabel(f"attribute {args.attribute[0]}", labelpad = 20, fontsize = 16)
-    plt.ylabel(f"attribute {args.attribute[1]}", labelpad = 20, fontsize = 16)
+    plt.xlabel(f"{attribute_names[int(args.attribute[0])]}", labelpad = 20)
+    plt.ylabel(f"{attribute_names[int(args.attribute[1])]}", labelpad = 20)
     cbar = plt.colorbar()
-    cbar.set_label(label = r"log$_{10}$($\Phi$)", fontsize = 16)
+    cbar.set_label(label = r"log$_{10}$($\Phi$)")
     # plt.axis('off')
     plt.xticks([], [])
     plt.yticks([], [])
@@ -114,30 +123,30 @@ if args.input == "ps":
     #             text = plt.text(j, i, np.round(table["pattern spectrum"][0][i, j], 1), ha="center", va="center", color="black", fontsize = 6)
 
     plt.tight_layout()
-    print(path_tif + filename_tif + ".tif")
-    plt.savefig(path_tif + filename_tif + ".tif", dpi = 150)
+    print(path_pdf + filename_pdf + ".pdf")
+    plt.savefig(path_pdf + filename_pdf + ".pdf", dpi = 150)
     plt.close()
 
 elif args.input == "cta":
-    path_tif = f"dm-finder/data/{args.particle_type}/images/" + run_filename + "/float" + f"/{args.telescope_mode}" + "/obs_id_" + f"{table['obs_id'][0]}/tif/"
+    path_pdf = f"dm-finder/data/{args.particle_type}/images/" + run_filename + "/float" + f"/{args.telescope_mode}" + "/obs_id_" + f"{table['obs_id'][0]}/pdf/"
 
     if args.tel_id == None:
-        filename_tif = "obs_id_" + f"{table['obs_id'][0]}" + "__event_id_" + f"{table['event_id'][0]}"
+        filename_pdf = "obs_id_" + f"{table['obs_id'][0]}" + "__event_id_" + f"{table['event_id'][0]}"
     else:
-        filename_tif = "obs_id_" + f"{table['obs_id'][0]}" + "__event_id_" + f"{table['event_id'][0]}" + "__tel_id_" + f"{table['tel_id'][0]}"
+        filename_pdf = "obs_id_" + f"{table['obs_id'][0]}" + "__event_id_" + f"{table['event_id'][0]}" + "__tel_id_" + f"{table['tel_id'][0]}"
 
-    os.makedirs(path_tif, exist_ok = True)
+    os.makedirs(path_pdf, exist_ok = True)
 
-    plt.figure()
+    plt.figure(figsize = single_column_fig_size)
 
     if args.tel_id == None:
-        plt.title("obs " + f"{table['obs_id'][0]}" + " - event " + f"{table['event_id'][0]}", fontsize = 16, pad = 20)
+        plt.title("obs " + f"{table['obs_id'][0]}" + " - event " + f"{table['event_id'][0]}", pad = 20)
     else:
-        plt.title("obs " + f"{table['obs_id'][0]}" + " - event " + f"{table['event_id'][0]}" + " - tel " + f"{table['tel_id'][0]}", fontsize = 16, pad = 20)
+        plt.title("obs " + f"{table['obs_id'][0]}" + " - event " + f"{table['event_id'][0]}" + " - tel " + f"{table['tel_id'][0]}", pad = 20)
 
     plt.imshow(table["image"][0], cmap = "Greys_r") 
     cbar = plt.colorbar()
-    cbar.set_label(label = "photon count")
+    cbar.set_label(label = "Photon count")
     plt.xlabel("x")
     plt.ylabel("y")
     plt.xticks([0, 10, 20, 30, 40])
@@ -145,6 +154,6 @@ elif args.input == "cta":
     bbox_inches = None
     pad_inches = 0.1
     plt.tight_layout()
-    print(path_tif + filename_tif + ".tif")
-    plt.savefig(path_tif + filename_tif + ".tif", bbox_inches = bbox_inches, pad_inches = pad_inches, dpi = 150)
+    print(path_pdf + filename_pdf + ".pdf")
+    plt.savefig(path_pdf + filename_pdf + ".pdf", bbox_inches = bbox_inches, pad_inches = pad_inches, dpi = 150)
     plt.close()
